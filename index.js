483ffc1d953f053778da3c2c88cdf6cac2cd7aee
高级捣蛋师 <yokins.shi@icloud.com>
2024-02-20 09:42:59 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-import { toHumanMoney, toRealMoney, deepCopy } from "./lib/common";
+import {
+    toHumanMoney,
+    toRealMoney,
+    deepCopy,
+    dateToSec,
+    dateToMil,
+} from "./lib/common";
 import { generateHttp } from "./lib/http";
 import { Mqtt } from "./lib/mqtt";
 
-export { toHumanMoney, toRealMoney, deepCopy, generateHttp, Mqtt };
+export {
+    toHumanMoney,
+    toRealMoney,
+    deepCopy,
+    dateToSec,
+    dateToMil,
+    generateHttp,
+    Mqtt,
+};
